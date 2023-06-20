ba4d6786f6b516bd79cd70d22d3b5a2dac17aacb
Ben-Luften-Lau <sirluftenlau@gmail.com>
2023-06-20 19:12:44 +0800

    render arrow

--- components/Mainpage/Custombttn.tsx
+++ components/Mainpage/Custombttn.tsx
@@ -1,19 +1,34 @@
-'use client'
-import { CustomBttnProps } from "@/types"
-import Image from "next/image"
+"use client";
+import { CustomBttnProps } from "@/types";
+import Image from "next/image";
 
-export default function Custombttn({title,containerStyles,handleClick,btnType,rightIcon,textStyles }:CustomBttnProps) {
+export default function Custombttn({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  rightIcon,
+  textStyles,
+  disable,
+}: CustomBttnProps) {
   return (
-    <button 
-        disabled={false}
-        type={btnType || "button"}
-        className={`custom-btn ${containerStyles}`}
-        onClick={handleClick}
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
     >
-
-        <span className={"flex-1"}>
-            {title}
-        </span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className=" relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right Icon"
+            fill
+            className=" object-contain"
+          />
+        </div>
+      )}
     </button>
-  )
+  );
 }
