4c7d77ebbdb51f919783b5ce7b16a73654f923be
Ben-Luften-Lau <sirluftenlau@gmail.com>
2023-06-22 08:15:26 +0800

    Car Drtails Modal Complete

--- components/carCard/CarDetails.tsx
+++ components/carCard/CarDetails.tsx
@@ -2,6 +2,7 @@ import { CarDetialsProps } from "@/types";
 import Image from "next/image";
 import React, { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { split } from "postcss/lib/list";
 
 export default function CarDetails({
   isOpen,
@@ -36,7 +37,7 @@ export default function CarDetails({
               >
                 <Dialog.Panel
                   className={
-                    "relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left  shadow-xl transition-all flex-col gap-5"
+                    "relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left  shadow-xl transition-all flex-col gap-5"
                   }
                 >
                   <button
@@ -72,6 +73,40 @@ export default function CarDetails({
                           className="object-contain"
                         />
                       </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={"/hero.png"}
+                          alt="car modal"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={"/hero.png"}
+                          alt="car modal"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2 className="font-semibold text-xl capitalize">
+                      {car.make} {car.model}
+                    </h2>
+                    <div className=" mt-3 flex flex-wrap gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div
+                          className="flex justify-between gap-5 w-full text-right"
+                          key={key}
+                        >
+                          <h4 className="text-grey capitalize">{key.split('_').join(" ")}</h4>
+                          <p className=" text-black-100 font-semibold">{value}</p>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </Dialog.Panel>
