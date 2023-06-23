35d483a91c73f040fc8de45a60f0000b70a206c9
Ben-Luften-Lau <sirluftenlau@gmail.com>
2023-06-23 17:50:28 +0800

    Trying to fix the problem changing config from docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const { hostname } = require('os')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        remotePatterns: [
+            {
+                protocol:'https' ,
+                hostname:'cdn.imagin.studio' ,
+            }
+        ],
         domains:["cdn.imagin.studio"]
     }
 }
